2f3d8267647133a15d429b126d545429498774c1
nadiaskau <nadiaskau@hotmail.com>
2021-03-15 13:28:26 +0100

    view of books done (with API)

--- public/script/page.js
+++ public/script/page.js
@@ -14,18 +14,79 @@ const getBooks = function(ev) {
 const showBooks = function(e){
     console.log('showbooks');
     let element = $("books");
+    
     while (element.firstChild) {
         element.removeChild(element.firstChild);
     }
-
+    
     let books = JSON.parse(e.target.responseText);
+    
+    //Iterate through all books
     books.forEach(function(book) {
-        let opt = document.createElement('p');
+        let div = document.createElement('div');
+        let title = document.createTextNode(book.title + ": ");
+        div.appendChild(title);
         
-        let opttext = document.createTextNode(book.title);
-        opt.appendChild(opttext);
-        element.appendChild(opt);
+        //Iterate through authors 
+        book.authors.forEach(function(author){
+            console.log(author);
+            let authortxt = document.createTextNode(`${author.firstname} ${author.middlename} ${author.lastname}, `);
+            div.appendChild(authortxt);
+        })
+
+        //Create and append detailsbutton
+        let buttontxt = document.createTextNode("Details");
+        let button = document.createElement('button');
+        button.appendChild(buttontxt);
+        button.addEventListener('click', function(){
+            showDetails(books, book.title);
+        }); 
+
+        //Create and append loanbutton
+        let buttontxtLoan = document.createTextNode("Loan");
+        let buttonLoan = document.createElement('button');
+        buttonLoan.appendChild(buttontxtLoan);
+        buttonLoan.addEventListener('click', function(){
+            //Loan
+        }); 
+
+        //Create and append reservebutton
+        let buttontxtReserve = document.createTextNode("Reserve");
+        let buttonReserve = document.createElement('button');
+        buttonReserve.appendChild(buttontxtReserve);
+        buttonReserve.addEventListener('click', function(){
+            //Reserve 
+        }); 
+
+        div.appendChild(button);
+        div.appendChild(buttonLoan);
+        div.appendChild(buttonReserve);
+        element.appendChild(div);
+
+        let details = document.createElement('div');
+        details.setAttribute('id', book.title);
+        div.appendChild(details);
+
     });
+
+}
+
+const showDetails = function(books, title){
+    let book; 
+    let divelement = document.getElementById(title);
+    while (divelement.firstChild) {
+        divelement.removeChild(divelement.firstChild);
+    }
+    //Searching for the title
+    for (var i=0; i < books.length; i++) {
+        if (books[i].title === title) {
+            book = books[i];
+        }
+    }
+
+    divelement.innerHTML = `Publisher: ${book.published.publisher}, Place: ${book.published.place}, Year: ${book.published.year}
+    <br> Copyright: ${book.copyright}
+    <br> Edition: ${book.edition}`; 
 
 }
 
